409a89ab12a652053d13e58fb190520e66aaa403
Pedro Terraf <pedroterraf@gmail.com>
2023-10-04 12:21:42 -0300

    Update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,7 @@
 import './App.scss'
-<<<<<<< HEAD
-import { Routes, Route, useLocation } from "react-router-dom";
-import Navbar from './components/Navbar/Navbar';
-=======
 /* import {useLocation} from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar'; */
 import { Routes, Route } from "react-router-dom";
->>>>>>> 2ca11b403522fbab79aafbca0df7bde18e4b2c3d
 import Landing from './views/Landing/Landing';
 import Home from './views/Home/Home';
 import Detail from './views/Detail/Detail';
@@ -16,14 +11,13 @@ function App() {
   /* const location = useLocation(); */
   return (
     <> 
-    {/* {location.pathname !== '/' && <Navbar />} */}
-    <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/details/:id" element={<Detail />} />
-      <Route path='/*' element={<Error />} />
-    </Routes>
-      
+      {/* {location.pathname !== '/' && <Navbar />} */}
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/details/:id" element={<Detail />} />
+        <Route path='/*' element={<Error />} />
+      </Routes>
     </>
   )
 }
