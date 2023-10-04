0c4ae085abd70253dcbd9229ee2e60178d2ba346
Juan <juampi.parzybal@gmail.com>
2023-10-04 13:28:28 -0300

    feat(redux): store and first slice

--- client/src/redux/store/store.ts
+++ client/src/redux/store/store.ts
@@ -1,1 +1,12 @@
-//store de redux
+import { configureStore } from "@reduxjs/toolkit";
+import { productSlice } from "../slices/productSlice";
+import { useDispatch, useSelector, TypedUseSelectorHook } from 'react-redux'
+
+export const store = configureStore({
+    reducer: {
+        products: productSlice.reducer
+    }
+})
+
+export const useAppDispatch:()=> typeof store.dispatch = useDispatch;
+export const useAppSelector:TypedUseSelectorHook<ReturnType<typeof store.getState>>=useSelector;
