d4090476dad685580213d1411d708b28b624cf85
andrescimma1 <andrescimma1@gmail.com>
2023-10-04 17:34:44 -0300

    #SUP-19 #time 30m

---
+++ client/src/pages/detail/Detail.tsx
@@ -0,0 +1,7 @@
+import SwipeableTextMobileStepper from "./components/Swipeable";
+
+const Detail = () => {
+  return <SwipeableTextMobileStepper />;
+};
+
+export default Detail;
---
+++ client/src/pages/detail/components/Swipeable.tsx
@@ -0,0 +1,105 @@
+import * as React from "react";
+import { useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import MobileStepper from "@mui/material/MobileStepper";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
+
+const images = [
+  {
+    label: "San Francisco – Oakland Bay Bridge, United States",
+    imgPath:
+      "https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60",
+  },
+  {
+    label: "Bird",
+    imgPath:
+      "https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60",
+  },
+  {
+    label: "Bali, Indonesia",
+    imgPath:
+      "https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250",
+  },
+  {
+    label: "Goč, Serbia",
+    imgPath:
+      "https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60",
+  },
+];
+
+function SwipeableTextMobileStepper() {
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = React.useState(0);
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => (prevActiveStep + 1) % images.length);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) =>
+      prevActiveStep === 0 ? images.length - 1 : prevActiveStep - 1
+    );
+  };
+
+  return (
+    <Box sx={{ maxWidth: 400, flexGrow: 1 }}>
+      <Paper
+        square
+        elevation={0}
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          height: 50,
+          pl: 2,
+          bgcolor: "background.default",
+        }}
+      >
+        <Typography>{images[activeStep].label}</Typography>
+      </Paper>
+      <div>
+        <img
+          style={{
+            height: 255,
+            display: "block",
+            maxWidth: 400,
+            overflow: "hidden",
+            width: "100%",
+          }}
+          src={images[activeStep].imgPath}
+          alt={images[activeStep].label}
+        />
+      </div>
+      <MobileStepper
+        steps={images.length}
+        position="static"
+        activeStep={activeStep}
+        nextButton={
+          <Button size="small" onClick={handleNext}>
+            Next
+            {theme.direction === "rtl" ? (
+              <KeyboardArrowLeft />
+            ) : (
+              <KeyboardArrowRight />
+            )}
+          </Button>
+        }
+        backButton={
+          <Button size="small" onClick={handleBack}>
+            {theme.direction === "rtl" ? (
+              <KeyboardArrowRight />
+            ) : (
+              <KeyboardArrowLeft />
+            )}
+            Back
+          </Button>
+        }
+      />
+    </Box>
+  );
+}
+
+export default SwipeableTextMobileStepper;
