f083b95bbb3d8df60e538f820428c24e693a1aba
Pedro Terraf <pedroterraf@gmail.com>
2023-10-03 21:56:09 -0300

    second commit

--- client/src/views/Home/Home.tsx
+++ client/src/views/Home/Home.tsx
@@ -1,8 +1,11 @@
+//import Navbar from '../../components/Navbar/Navbar'
 import './Home.scss'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <h1>HOLAAA</h1>
+    </div>
   )
 }
 export default Home
